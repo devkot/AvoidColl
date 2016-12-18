739c257deeda0b96a114eb98af88f13b0fd8f03b
Kotanidis <sdi1300236@di.uoa.gr>
2016-12-18 13:40:28 +0200

    google api

--- app/src/main/java/com/example/stelios/ergasia1/OnMode.java
+++ app/src/main/java/com/example/stelios/ergasia1/OnMode.java
@@ -10,8 +10,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
-
-import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
